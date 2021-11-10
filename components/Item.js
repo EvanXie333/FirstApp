6462d37555ee5b1052d8f6dccf4f61f5e32bd490
evanxie <1007979078@q.com>
2021-11-10 14:56:44 +1100

    use icon image to replace text in button

--- components/Item.js
+++ components/Item.js
@@ -1,13 +1,19 @@
 import React from "react"
-import { View, Text, StyleSheet, Button } from "react-native"
+import { View, Text, StyleSheet, Button,Image,TouchableOpacity } from "react-native"
 export const Item =(props) =>{
 
     
     return(
         <View style={styles.container}>
             <Text style={(props.status) ? styles.textDone : styles.text}>{props.text}</Text>
-            <Button title="delete" onPress={ ()=> props.delete(props.id)}/>
-            <Button title="done" onPress ={ ()=> props.done(props.id)}/>
+            <TouchableOpacity onPress ={ ()=> props.done(props.id)}>
+                <Image style={styles.icon} source ={require('../assets/check-solid.png') }/>
+            </TouchableOpacity>
+            <TouchableOpacity onPress ={ ()=> props.delete(props.id)}>
+                <Image style={styles.icon} source ={require('../assets/trash-alt-solid.png') }/>
+            </TouchableOpacity>
+            {/* <Button title="delete" onPress={ ()=> props.delete(props.id)}/> */}
+            {/* <Button title="done" onPress ={ ()=> props.done(props.id)}/> */}
         </View>
     )
 }
@@ -30,5 +36,9 @@ export const Item =(props) =>{
           flex:1,
           textDecorationLine:"line-through",
           color:"gray",
+      },
+      icon:{
+          width:30,
+          height:30,
       }
   })
